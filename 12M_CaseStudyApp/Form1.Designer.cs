4994de579f7ea46a79cfda85f603c0a50f728711
Berkaycan Sakar <berkaycansakar@gmail.com>
2023-10-10 13:36:54 +0300

    Add project files.

---
+++ 12M_CaseStudyApp/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace _12M_CaseStudyApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.lbl_baslangicTarihi = new DevExpress.XtraEditors.LabelControl();
+            this.lbl_bitisTarihi = new DevExpress.XtraEditors.LabelControl();
+            this.lbl_malKodu = new DevExpress.XtraEditors.LabelControl();
+            this.cmbx_malKodu = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.datePicker_baslangicTarihi = new System.Windows.Forms.DateTimePicker();
+            this.datePicker_bitisTarihi = new System.Windows.Forms.DateTimePicker();
+            this.btn_yazdir = new DevExpress.XtraEditors.SimpleButton();
+            this.btn_excel = new DevExpress.XtraEditors.SimpleButton();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbx_malKodu.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(131, 130);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(546, 330);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            // 
+            // lbl_baslangicTarihi
+            // 
+            this.lbl_baslangicTarihi.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_baslangicTarihi.Appearance.Options.UseFont = true;
+            this.lbl_baslangicTarihi.Location = new System.Drawing.Point(16, 26);
+            this.lbl_baslangicTarihi.Name = "lbl_baslangicTarihi";
+            this.lbl_baslangicTarihi.Size = new System.Drawing.Size(104, 16);
+            this.lbl_baslangicTarihi.TabIndex = 6;
+            this.lbl_baslangicTarihi.Text = "Başlangıç Tarihi:";
+            // 
+            // lbl_bitisTarihi
+            // 
+            this.lbl_bitisTarihi.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_bitisTarihi.Appearance.Options.UseFont = true;
+            this.lbl_bitisTarihi.Location = new System.Drawing.Point(297, 26);
+            this.lbl_bitisTarihi.Name = "lbl_bitisTarihi";
+            this.lbl_bitisTarihi.Size = new System.Drawing.Size(71, 16);
+            this.lbl_bitisTarihi.TabIndex = 7;
+            this.lbl_bitisTarihi.Text = "Bitiş Tarihi:";
+            // 
+            // lbl_malKodu
+            // 
+            this.lbl_malKodu.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_malKodu.Appearance.Options.UseFont = true;
+            this.lbl_malKodu.Location = new System.Drawing.Point(16, 61);
+            this.lbl_malKodu.Name = "lbl_malKodu";
+            this.lbl_malKodu.Size = new System.Drawing.Size(92, 16);
+            this.lbl_malKodu.TabIndex = 8;
+            this.lbl_malKodu.Text = "Mal Kodu/Adı:";
+            // 
+            // cmbx_malKodu
+            // 
+            this.cmbx_malKodu.Location = new System.Drawing.Point(114, 60);
+            this.cmbx_malKodu.Name = "cmbx_malKodu";
+            this.cmbx_malKodu.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cmbx_malKodu.Size = new System.Drawing.Size(317, 20);
+            this.cmbx_malKodu.TabIndex = 3;
+            this.cmbx_malKodu.SelectedIndexChanged += new System.EventHandler(this.cmbx_malKodu_SelectedIndexChanged);
+            this.cmbx_malKodu.TextChanged += new System.EventHandler(this.cmbx_malKodu_TextChanged);
+            // 
+            // datePicker_baslangicTarihi
+            // 
+            this.datePicker_baslangicTarihi.Location = new System.Drawing.Point(126, 24);
+            this.datePicker_baslangicTarihi.Name = "datePicker_baslangicTarihi";
+            this.datePicker_baslangicTarihi.Size = new System.Drawing.Size(156, 21);
+            this.datePicker_baslangicTarihi.TabIndex = 1;
+            this.datePicker_baslangicTarihi.Value = new System.DateTime(2016, 1, 1, 13, 22, 0, 0);
+            this.datePicker_baslangicTarihi.ValueChanged += new System.EventHandler(this.datePicker_baslangicTarihi_ValueChanged);
+            // 
+            // datePicker_bitisTarihi
+            // 
+            this.datePicker_bitisTarihi.Location = new System.Drawing.Point(374, 22);
+            this.datePicker_bitisTarihi.Name = "datePicker_bitisTarihi";
+            this.datePicker_bitisTarihi.Size = new System.Drawing.Size(156, 21);
+            this.datePicker_bitisTarihi.TabIndex = 2;
+            this.datePicker_bitisTarihi.ValueChanged += new System.EventHandler(this.datePicker_bitisTarihi_ValueChanged);
+            // 
+            // btn_yazdir
+            // 
+            this.btn_yazdir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_yazdir.ImageOptions.AllowGlyphSkinning = DevExpress.Utils.DefaultBoolean.False;
+            this.btn_yazdir.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btn_yazdir.ImageOptions.Image")));
+            this.btn_yazdir.ImageOptions.Location = DevExpress.XtraEditors.ImageLocation.TopCenter;
+            this.btn_yazdir.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btn_yazdir.Location = new System.Drawing.Point(493, 52);
+            this.btn_yazdir.Name = "btn_yazdir";
+            this.btn_yazdir.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            this.btn_yazdir.Size = new System.Drawing.Size(37, 38);
+            this.btn_yazdir.TabIndex = 4;
+            this.btn_yazdir.Click += new System.EventHandler(this.btn_yazdir_Click);
+            // 
+            // btn_excel
+            // 
+            this.btn_excel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_excel.ImageOptions.AllowGlyphSkinning = DevExpress.Utils.DefaultBoolean.False;
+            this.btn_excel.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btn_excel.ImageOptions.Image")));
+            this.btn_excel.ImageOptions.Location = DevExpress.XtraEditors.ImageLocation.TopCenter;
+            this.btn_excel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btn_excel.Location = new System.Drawing.Point(450, 52);
+            this.btn_excel.Name = "btn_excel";
+            this.btn_excel.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            this.btn_excel.Size = new System.Drawing.Size(37, 38);
+            this.btn_excel.TabIndex = 9;
+            this.btn_excel.Click += new System.EventHandler(this.btn_excel_Click);
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.cmbx_malKodu);
+            this.panelControl1.Controls.Add(this.btn_excel);
+            this.panelControl1.Controls.Add(this.lbl_baslangicTarihi);
+            this.panelControl1.Controls.Add(this.btn_yazdir);
+            this.panelControl1.Controls.Add(this.lbl_bitisTarihi);
+            this.panelControl1.Controls.Add(this.datePicker_bitisTarihi);
+            this.panelControl1.Controls.Add(this.lbl_malKodu);
+            this.panelControl1.Controls.Add(this.datePicker_baslangicTarihi);
+            this.panelControl1.Location = new System.Drawing.Point(130, 24);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(547, 100);
+            this.panelControl1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 534);
+            this.Controls.Add(this.panelControl1);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "12M Consulting";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbx_malKodu.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.LabelControl lbl_baslangicTarihi;
+        private DevExpress.XtraEditors.LabelControl lbl_bitisTarihi;
+        private DevExpress.XtraEditors.LabelControl lbl_malKodu;
+        private DevExpress.XtraEditors.ComboBoxEdit cmbx_malKodu;
+        private System.Windows.Forms.DateTimePicker datePicker_baslangicTarihi;
+        private System.Windows.Forms.DateTimePicker datePicker_bitisTarihi;
+        private DevExpress.XtraEditors.SimpleButton btn_yazdir;
+        private DevExpress.XtraEditors.SimpleButton btn_excel;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+    }
+}
+
---
+++ 12M_CaseStudyApp/Form1.cs
@@ -0,0 +1,118 @@
+﻿using DevExpress.Export;
+using DevExpress.XtraPrinting;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace _12M_CaseStudyApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connectionString = "Data Source=LAPTOP-BEKO;Initial Catalog=TEST;Integrated Security=True";
+        string currentKod = "";
+        int baslangic_Tarihi = 0;
+        int bitis_Tarihi= 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Form yüklendiğinde datepicker üzerinden tarih bilgisi alınır
+            baslangic_Tarihi = Convert.ToInt32((datePicker_baslangicTarihi.Value.Date).ToOADate());
+            bitis_Tarihi = Convert.ToInt32((datePicker_bitisTarihi.Value.Date).ToOADate());
+        }
+
+        private DataTable List_MalKod(string searchText)
+        {
+            DataTable table = new DataTable(); // Verileri saklamak için datatable oluşturulur.
+            using (SqlConnection connection = new SqlConnection(connectionString)) //Veri tabanına bağlanmak için connection nesnesi oluşturulur.
+            {
+                connection.Open(); //veritabanı bağlantısı açılır.
+                string query = "SELECT MalKodu, MalAdi FROM STK WHERE MalKodu LIKE @searchText OR MalAdi LIKE @searchText"; //ilgili veriyi getirecek sql sorgusu yazılır.
+                SqlCommand command = new SqlCommand(query, connection);// sorgu ve bağlantı command a tanımlanır.
+                command.Parameters.AddWithValue("@searchText", "%" + searchText + "%");//parametre doldurulur.
+
+                using (SqlDataReader reader = command.ExecuteReader()) // sorgu çalıştırılır. Reader ile sorgu sonucu okunarak table üzerine yüklenir.
+                {
+                    table.Load(reader);
+                }
+                connection.Close(); // bağlantı kapatılır.
+            }
+            return table;
+        }
+   
+        private void FillGrid(string malKod)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                DataTable resultTable = new DataTable();
+                SqlCommand command = new SqlCommand("usp_GetDataList", connection);
+                command.CommandType = CommandType.StoredProcedure;//Sorgu türünün store procedure olduğu tanımlanır.
+                command.Parameters.AddWithValue("@MalKodu", malKod);// parametre değerleri verilir.
+                command.Parameters.AddWithValue("@Baslangic_Tarihi", baslangic_Tarihi);
+                command.Parameters.AddWithValue("@Bitis_Tarihi", bitis_Tarihi);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);// Veriler adapter ile çekilir
+                adapter.Fill(resultTable); // ve datatable üzerine doldurulur.
+               
+                gridControl1.DataSource=resultTable; // Gridcontrol üzerinde datalar listelenir.
+                connection.Close();
+            }
+        }
+        private void cmbx_malKodu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //combobox itemleri temizlenir.
+            currentKod = cmbx_malKodu.Text; //combobox texti alınır.
+            FillGrid(currentKod); // grid güncellenir.
+        }
+        private void cmbx_malKodu_TextChanged(object sender, EventArgs e)
+        {
+            cmbx_malKodu.Properties.Items.Clear(); // combobox itemleri temizlenir.
+
+            string searchText = cmbx_malKodu.Text;
+            DataTable searchTable = List_MalKod(searchText);  //Girilen mal kodu/adi  na göre mal kodları getirilir. 
+
+            // Table üzerindeki tüm satırlar döndürülür. Ve Malkodu sütunu comboboxa item olarak verilir.
+            foreach (DataRow row in searchTable.Rows)  
+            {
+                string kod = row["MalKodu"].ToString();
+                cmbx_malKodu.Properties.Items.Add(kod);
+            }
+            cmbx_malKodu.ShowPopup();// combobox items listesi açılır.
+        }
+        private void datePicker_baslangicTarihi_ValueChanged(object sender, EventArgs e)
+        {
+            //datepicker üzerinden tarih bilgisi int olarak alınır
+           baslangic_Tarihi = Convert.ToInt32((datePicker_baslangicTarihi.Value.Date).ToOADate());
+            if (currentKod != "") // Eğer malodu girilmişse grid doldurulur.
+                FillGrid(currentKod);
+        }
+        private void datePicker_bitisTarihi_ValueChanged(object sender, EventArgs e)
+        {
+            bitis_Tarihi = Convert.ToInt32((datePicker_bitisTarihi.Value.Date).ToOADate());
+            if(currentKod != "")
+                FillGrid(currentKod);
+        }
+        private void btn_yazdir_Click(object sender, EventArgs e)
+        {
+            //Yazdırma önizlemesini görüntülenir.
+            gridControl1.ShowPrintPreview();
+        }
+        private void btn_excel_Click(object sender, EventArgs e)
+        {
+            //Dosya kaydetme işlemi için bir dosya seçme penceresi açar.
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel Files|*.xlsx";       //Kaydedilecek dosyanın türünü filtreler
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK) // kaydet butonu tıklanmışsa
+            {
+                XlsxExportOptionsEx options = new XlsxExportOptionsEx();
+                options.ExportType = ExportType.WYSIWYG;// Option olarak dışarıya verilerin göründüğü gibi aktarılması gerektiği bildirilir..
+                gridView1.ExportToXlsx(saveFileDialog.FileName, options);// Excel olarak aktarılır.
+            }
+        }
+    }
+}
